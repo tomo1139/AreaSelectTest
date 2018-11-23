f3c4565568b2bfddb820b305859adec924a0cb74
beta1139 <beta1139@gmail.com>
2018-11-23 23:21:34 +0900

    no message

--- AreaSelectTest/app/src/main/java/com/example/tomo/areaselecttest/MainActivity.kt
+++ AreaSelectTest/app/src/main/java/com/example/tomo/areaselecttest/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.tomo.areaselecttest
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tomo.areaselecttest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding
     }
 }
